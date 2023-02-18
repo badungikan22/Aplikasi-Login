68ac56bf559ff423967f36b8074262be6eca1dfa
badungikan22 <122475155+badungikan22@users.noreply.github.com>
2023-02-18 13:50:10 +0700

    Update login-script.js
    mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,21 +1,45 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/**
+*@constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+
+/**
+*@constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/**
+*@constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+*@constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+
+/**
+*@constant {string}
+*/
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  
+  /**
+*@constant {string}
+*/
   const email = inputEmailElement.value;
+  
+  /**
+*@constant {string}
+*/
   const password = inputPasswordElement.value;
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
